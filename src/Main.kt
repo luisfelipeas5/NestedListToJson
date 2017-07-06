3f1693f20d922f57b4b5bdcc251a56295835229a
Luís Felipe <luis.almeida@madeinweb.com.br>
2017-07-06 14:19:06 -0300

    Regex fixed

--- src/Main.kt
+++ src/Main.kt
@@ -4,39 +4,38 @@ import java.util.regex.Pattern
 import java.util.*
 
 class NestedList {
-    var key: String? = null
+    var description: String? = null
     var nestedLists: MutableList<NestedList> = LinkedList()
+    var id: String? = null
 }
 
 fun main(args: Array<String>) {
     val decimalPointRegex = "(\\d*\\.).*"
 
     val file = File("./src/nestedlist.txt")
-    val regexList = arrayOf(decimalPointRegex, "(^(?!i)[a-z]\\..*$)", "(i*\\.).*")
-    printNestedListFileAsJson(file, regexList)
-
-    println()
-    println()
-    println()
-    println()
-    println()
+    val regexList = arrayOf(decimalPointRegex, "([a-z]\\..*$)", "([iv]*\\.).*")
+    val nestedList = getNestedListFileAsJson(file, regexList)
 
     val file2 = File("./src/nestedlist2.txt")
     val regexList2 = arrayOf(decimalPointRegex, "(^[a-z]\\..*$)")
-    printNestedListFileAsJson(file2, regexList2)
+    val nestedList1 = getNestedListFileAsJson(file2, regexList2)
+
+    nestedList.nestedLists.addAll(nestedList1.nestedLists)
+    printObjectAsJson(nestedList)
 }
 
-fun printNestedListFileAsJson(file: File, regexList: Array<String>) {
+fun getNestedListFileAsJson(file: File, regexList: Array<String>): NestedList {
     val lines = file.readLines(Charset.defaultCharset())
     val nestedList = NestedList()
     fillNestedRoot(lines.listIterator(), regexList, 0, nestedList)
-    printObjectAsJson(nestedList)
+    return nestedList
 }
 
 fun printObjectAsJson(nestedList: NestedList) {
     println("{")
 
-    println("\"key\": \"" + nestedList.key + "\",")
+    println("\"key\": \"" + nestedList.id + "\",")
+    println("\"description\": \"" + nestedList.description + "\",")
     val subNestedLists = nestedList.nestedLists
 
     println("\"list\": [")
@@ -77,7 +76,7 @@ private fun fillNestedRoot(linesIterator: ListIterator<String>, regexList: Array
 
             linesIterator.previous()
             if (!fillNestedRoot(linesIterator, regexList, regexIndex + 1, subNestedList) && linesIterator.hasNext()) {
-                subNestedList.key += line
+                subNestedList.description += line
                 linesIterator.next()
             }
 
@@ -94,7 +93,17 @@ fun putNestedLine(group: String, regexIndex: Int, rootNestedList: NestedList): N
         i++
     }
     val nestedList = NestedList()
-    nestedList.key = group
+
+    val pattern = Pattern.compile("(.*)\\.(.*)")
+    val matcher = pattern.matcher(group)
+    val find = matcher.find()
+    if (find) {
+        val id = matcher.group(1)
+        nestedList.id = id
+
+        val description = matcher.group(2)
+        nestedList.description = description
+    }
 
     rootNestedList.nestedLists.add(nestedList)
     return nestedList
